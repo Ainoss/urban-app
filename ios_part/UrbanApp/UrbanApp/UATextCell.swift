ee61a3d99cd31350d7b1ca81526bb041202bc39d
SarginD <avterks@gmail.com>
2015-04-26 02:25:41 +0300

    adding text messages

---
+++ ios_part/UrbanApp/UrbanApp/UATextCell.swift
@@ -0,0 +1,14 @@
+//
+//  UATextCell.swift
+//  UrbanApp
+//
+//  Created by Daniil Sargin on 26/04/15.
+//  Copyright (c) 2015 Даниил Саргин. All rights reserved.
+//
+
+import UIKit
+
+class UATextCell: UITableViewCell {
+    @IBOutlet weak var messageText: UILabel!
+    @IBOutlet weak var timeText: UILabel!
+}
