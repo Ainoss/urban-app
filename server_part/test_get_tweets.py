2261badf321820d7477aa0e38543842a7da40b46
AleksK <avkhatskevich@gmail.com>
2015-04-26 09:12:00 +0300

    Server

--- server_part/test_get_tweets.py
+++ server_part/test_get_tweets.py
@@ -5,6 +5,9 @@ from record import *
 from decision import *
 from time import time
 
+data_to_send = ""
+
+
 def start_getting_tweets():
     consumer_key = "KCZCsNS4OKwguAnnlZWUXXgI4"
     consumer_secret = "AfNy0WptjYQII5nb5DOSUEYSZvfoQnZc8nQXt4HmrUx5PwL9cP"
@@ -17,25 +20,35 @@ def start_getting_tweets():
     r = api.request('statuses/filter', {'locations':'37.3,55.5,37.9,55.9'})
     n = 0
     for item in r.get_iterator():
-        if 'text' in item:
-            print "-------"
-            print item['text']
+        array = make_decision()
+        temp = {"data": array}
+        temp = json.dumps( data_to_send ) + ' \n'
+        data_to_send = temp.replace(' ', '\n')
+        print data_to_send
+        print
+        #if 'text' in item:
+            #print "-------"
+            #print item['text']
         if 'coordinates' in item:
             if item['coordinates']:
-                print item['coordinates']['coordinates'][0]
-                print item['coordinates']['coordinates'][1]
-                set_record(item['coordinates']['coordinates'][1], item['coordinates']['coordinates'][0], item['text'], time())
+                #print item['coordinates']['coordinates'][0]
+                #print item['coordinates']['coordinates'][1]
+                record = Record()
+                record.latitude = item['coordinates']['coordinates'][1]
+                record.longitude = item['coordinates']['coordinates'][0]
+                record.message = item['text']
+                record.time = time();
+                set_record(record)
                 n += 1
-                f.write('n = ' + str(n) + '\n')
-                f.write(str(item['text'].encode('utf-8')) + '\n')
-                f.write(str(item['coordinates']['coordinates'][0]) + ' ')
-                f.write(str(item['coordinates']['coordinates'][1]) + '\n')
-                f.write(str(time()) + '\n')
-                f.write('\n')
+                #f.write('n = ' + str(n) + '\n')
+                #f.write(str(item['text'].encode('utf-8')) + '\n')
+                #f.write(str(item['coordinates']['coordinates'][0]) + ' ')
+                #f.write(str(item['coordinates']['coordinates'][1]) + '\n')
+                #f.write(str(time()) + '\n')
+                #f.write('\n')
                 #have to be in another thread
-                #make_decision()
 
 
 
-if __name__ == '__main__':
-    start_getting_tweets()
+#if __name__ == '__main__':
+#    start_getting_tweets()
